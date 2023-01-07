6148d43c316c8f5c9f71ff0191333cb6c53b2dea
fenugrec <fenugrec@users.sourceforge.net>
2023-01-07 16:17:41 -0500

    unpack_payload : process pre-stripped LIF file
    i.e. the HPFSLIF header (0x200 bytes) must already be removed.
    
    This version "works" but the last block is possibly not handled
    correctly.

---
+++ unpack_payload.py
@@ -0,0 +1,87 @@
+#!/bin/python3
+# (c) fenugrec 2023
+
+# assumes file already has the HPFSLIF header (0x200 bytes) stripped.
+
+
+
+from argparse import ArgumentParser
+import mmap
+
+def extract_blocks(fname, out_file):
+	with open(fname, "rb") as f:
+		mm = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
+
+	with open(out_file, "wb") as outf:
+		pl_len = 0
+		f_pos = 0
+		last_len = 0
+		while f_pos < mm.size():
+			mm.seek(f_pos)
+			block_len = (mm[f_pos] << 8) + mm[f_pos + 1]
+
+			#possible last-block marker ? it may be part 
+			if (block_len == 0xffff):
+				remaining_payload = mm.size() - f_pos - 2
+				print("lastblock ? {} more bytes; previous block {}".format(remaining_payload, last_len))
+				block_len = remaining_payload
+
+			if (block_len != 0xfe):
+				print("irregular block @ fileoffs {}: +{}".format(f_pos, block_len))
+
+			outf.write(mm[f_pos+2 : f_pos+2+block_len])
+			pl_len += block_len
+			f_pos += 2 + block_len
+			last_len = block_len
+
+		print("Payload size: {}, filesize {}, last pos {}".format(pl_len, mm.size(), f_pos))
+	return
+
+# Format : "XX YY <byte_0> <byte...> <byte_n>", where n = XXYY - 1 (block length is big-endian and refers to actual payload data)
+def list_blocks(fname):
+	with open(fname, "rb") as f:
+		mm = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
+
+	pl_len = 0
+	f_pos = 0
+	last_len = 0
+	while f_pos < mm.size():
+		mm.seek(f_pos)
+		block_len = (mm[f_pos] << 8) + mm[f_pos + 1]
+
+		#possible last-block marker ? it may be part 
+		if (block_len == 0xffff):
+			remaining_payload = mm.size() - f_pos - 2
+			print("lastblock ? {} more bytes; previous block {}".format(remaining_payload, last_len))
+			block_len = remaining_payload
+
+		if (block_len != 0xfe):
+			print("irregular block @ fileoffs {}: +{}".format(f_pos, block_len))
+		pl_len += block_len
+		f_pos += 2 + block_len
+		last_len = block_len
+
+	print("Payload size: {}, filesize {}, last pos {}".format(pl_len, mm.size(), f_pos))
+#	path.joinpath('patched').mkdir(exist_ok=1)
+
+	return
+
+def main():
+	parser = ArgumentParser()
+	parser.add_argument('fname', help="filename")
+	#parser.add_argument('-f', action="store_true", help="only find hashes, no modification")
+	parser.add_argument('-x', help="extract payload to specified output file")
+	parser.add_argument('-i', action="store_true", help="only print block info")
+	args = parser.parse_args()
+
+	#print(args)
+
+	if args.i:
+		list_blocks(args.fname)
+		return
+
+	if args.x:
+		extract_blocks(args.fname, args.x)
+
+if __name__ == '__main__':
+    main()
