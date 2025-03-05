845c667317d541f2d9ec897eaac70091c8fc2c19
fenugrec <fenugrec@users.sourceforge.net>
2025-03-05 16:43:28 -0500

    ialdown.py: more modernization
    -telnetlib3: pass string args as 'ascii' otherwise complains about wanting bytes
    -remove useless questions
    -specify R file in CLI instead of having to type it in

--- ialdown.py
+++ ialdown.py
@@ -10,10 +10,16 @@ import logging
 parser = argparse.ArgumentParser(description="Python IALDOWN for HP Logic Analysers")
 parser.add_argument('-H', '--host', required=True, help='LA hostname')
 parser.add_argument('-p', '--port', type=int, default=5025, help='defaults to 5025')
+parser.add_argument('-r', '--rfile', required=True, type=argparse.FileType('rb'), help='relocatable file to send')
 args = parser.parse_args(sys.argv[1:])
 
 hostname=args.host
 port=args.port
+rfile=args.rfile
+
+# read the data file
+buffer = rfile.read()
+
 
 # open up the telnet connection first
 # otherwise the user doesnt find out if its going to 
@@ -24,9 +30,6 @@ params = {}
 
 params['filename'] = input("Logic Analyzer Filename = ").rstrip()
 params['description'] = input("Logic Analyzer File Description\n(must be 32 characters or less) = ").rstrip()
-params['rfile'] = input("Relocatable File on the PC = ").rstrip()
-# ignored but here for compatibility with scripts
-params['comport'] = input("COM Port (Ignored) = ").rstrip()
 params['option'] = input(""""Invasm" Field Options:
  A = No "Invasm" Field
  B = "Invasm" Field with no pop-up
@@ -36,10 +39,6 @@ Select the appropriate letter (A, B, C or D)""").rstrip()
 
 if len(params['description']) > 32:
     print("Description too long will be trimmed")
-
-# read the data file
-with open(params['rfile'], 'rb') as f:
-    buffer = f.read()
 
 # get the params. 
 if params['option'].upper() == 'A':
@@ -54,11 +53,11 @@ else:
     raise Exception("Unknown type optiom specified (A,B,C or D)")
 
 # now actually do the transfer. 
-tn.write(""":MMEMory:DOWNload '{filename}',INTERNAL0,'{description}',-15614,""".format(**params))
+tn.write(""":MMEMory:DOWNload '{filename}',INTERNAL0,'{description}',-15614,""".format(**params).encode('ascii'))
 data_size = len(buffer)+1
 
-tn.write("#8%08i" % data_size)
-tn.write(type_byte)
+tn.write("#8%08i".encode('ascii') % data_size)
+tn.write(type_byte.encode('ascii'))
 tn.write(buffer)
-tn.write('\n')
+tn.write('\n'.encode('ascii'))
 tn.close()
