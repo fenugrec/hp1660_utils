c5e81bda8ebdb5955ae7927e146f2e6429b6b72e
fenugrec <fenugrec@users.sourceforge.net>
2025-03-10 11:46:37 -0400

    romdumper: capture loop works !!!1
    remaining issues :
     - can't safely abort and save progress so far
     - too verbose

--- romdumper_helper/batchcapture.py
+++ romdumper_helper/batchcapture.py
@@ -47,49 +47,9 @@ def set_darkmode (instr):
     for cn in range(1,8):
         instr.write(f":setc {cn},0,0,0")
 
-# stupid loop reading a chunk of data one line at a time...
-# slow !!!
-#
-# with proper triggering, HP line # would be 0...<2^n-1> e.g. 0-4095 or 0-8191 ;
-# n is # of lines to expect = 2^n
-# reply to query looks like '4095,"DATA  ","#H0008"'
-#
-# returns [[chunk0_start,b'...chunkdata..'],[chunk1_start...
-def get_chunk (instr, cnt, datawidth=2):
-    chunklist=[]
-    chunk_start = None
-    chunkdata = None
-    last_addr = None
-    numbytes = 0
-    for ln in range(0,cnt):
-        addr_text=instr.query(f":mach1:slist:data? {ln},'ADDR'")
-        # this parsing can probably be improved; can't believe pyvisa has nothing to help us here?
-        # query_ascii_values() almost works but uses the same formatter for all fields ?
-        addr=int(addr_text.split(',')[2].strip('"#H'),16)
-
-        data_text=instr.query(f":mach1:slist:data? {ln},'DATA'")
-        data=int(data_text.split(',')[2].strip('"#H'),16)
-#        print(f"a {addr:#x}, d={data:#x}")
-        if chunk_start is None:
-            #first loop only
-            chunk_start = addr
-            chunkdata = (data.to_bytes(datawidth))
-            last_addr = addr - 2
-
-        if addr == (last_addr + datawidth):
-            chunkdata = chunkdata + (data.to_bytes(datawidth))
-        else:
-            print(f"discontinuity from {last_addr:#x} to {addr:#x}")
-            chunklist.append([chunk_start, chunkdata])
-            chunk_start = addr
-            chunkdata = (data.to_bytes(datawidth))
-        last_addr = addr
-        numbytes += datawidth
-    print(f"read {numbytes:#x} bytes")
-    chunklist.append([chunk_start, chunkdata])
-    return chunklist
 
 # extract data
+# TODO: capture_depth should be automatic via parse_raw() or chunklist
 def dumploop (instr, start_addr, cnt, capture_depth, datawidth=2, timeout=5000):
     ca = start_addr
     chunks = []
@@ -98,15 +58,18 @@ def dumploop (instr, start_addr, cnt, capture_depth, datawidth=2, timeout=5000):
         instr.write(f":mach1:str:term b,'ADDR','#H{ca:x}'")
         instr.write('*cls')
         instr.write(':start')
-        # TODO : implement timeout + polling status + aborting beyond timeout
+        print(f"CAPTURE : waiting for trigger on addr={ca:#x}; reset target now")
+        # TODO : implement timeout + aborting
         while 1:
             esr = int(instr.query('mesr1?'))
             # bit 0 should be set when done
             if esr & 1: break
             # instr.write(':stop')
             time.sleep(0.2)
-        chunks.append(get_chunk(instr, cap, datawidth))
+        rd=get_rawdata(instr)
+        chunks.append(parse_raw(rd,am,dm))
         cnt -= cap
+        ca += capture_depth
     return chunks
 
 def write_chunks (fname, chunks):
@@ -165,7 +128,7 @@ def unshift_rawdata(src, mask):
 
 
 
-# parse raw data according to dev config
+# parse raw data according to dev config, return chunklist
 # maybe some work needed to make it less device-dependant
 # rd: raw data received from :SYST:DATA? query, starting at its "DATA      " header
 # _mask: (num_pods * 2)-bytes long mask of bits to extract data, e.g.
